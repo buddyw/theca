3b9aa56bb85c9c89f1a56f0e608788ba989875bd
f8 <1928558+buddyw@users.noreply.github.com>
2020-10-03 19:15:56 -0500

    Removed deprecated Error::description method

--- src/theca/errors.rs
+++ src/theca/errors.rs
@@ -95,7 +95,7 @@ impl From<EncoderError> for Error {
     fn from(err: EncoderError) -> Error {
         Error {
             kind: ErrorKind::Generic,
-            desc: err.description().to_string(),
+            desc: err.to_string(),
             detail: None,
         }
     }
@@ -105,7 +105,7 @@ impl From<IoError> for Error {
     fn from(err: IoError) -> Error {
         Error {
             kind: ErrorKind::Generic,
-            desc: err.description().into(),
+            desc: err.to_string().into(),
             detail: None,
         }
     }
@@ -114,7 +114,7 @@ impl From<IoError> for Error {
 impl From<term::Error> for Error {
     fn from(err: term::Error) -> Error {
         Error {
-            desc: err.description().into(),
+            desc: err.to_string().into(),
             kind: ErrorKind::Term(err),
             detail: None,
         }
@@ -135,7 +135,7 @@ impl From<SystemTimeError> for Error {
     fn from(err: SystemTimeError) -> Error {
         Error {
             kind: ErrorKind::Generic,
-            desc: err.description().into(),
+            desc: err.to_string().into(),
             detail: None,
         }
     }
